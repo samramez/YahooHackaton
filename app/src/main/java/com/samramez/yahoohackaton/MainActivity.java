beb51916450265605313bc6c84d431b142e5ad3e
VIkram <vikramgupta20.7@gmail.com>
2015-08-25 14:10:57 -0400

    GUI for selecting dates and search button

--- app/src/main/java/com/samramez/yahoohackaton/MainActivity.java
+++ app/src/main/java/com/samramez/yahoohackaton/MainActivity.java
@@ -3,6 +3,7 @@ package com.samramez.yahoohackaton;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -21,6 +22,12 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
+
+class Place{
+	String name;
+	int[] imageResource;
+}
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
@@ -29,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 	final int BUTTON_DEPART = 1;
 	final int BUTTON_RETURN = 2;
 	int buttonFlag;
+	Places places;
+	ArrayList<Place> placesList;
+	ListView listView;
+	ImageLoader imgLoader;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +50,37 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 		buttonDepart = (Button)findViewById(R.id.buttonDateDepart);
 		buttonReturn = (Button)findViewById(R.id.buttonDateReturn);
 		buttonDiscover = (Button)findViewById(R.id.buttonDiscover);
-		
+		listView = (ListView) findViewById(R.id.listViev);
+
 		buttonDepart.setOnClickListener(this);
 		buttonReturn.setOnClickListener(this);
 		buttonDiscover.setOnClickListener(this);
+
+		imgLoader = ImageLoader.getInstance();
+
+		popultatePlaces();
+
+		listView.setAdapter(new ImageAdapter());
 	}
+
+	public void popultatePlaces(){
+		place = new Place();
+		places.name = Miami;
+		places.imageResource = {R.drawable.slider1,R.drawable.slider2, R.drawable.slider3, R.drawable.slider4};
+		placesList.add(place);
+
+		place = new Place();
+		places.name = San Francisco;
+		places.imageResource = {R.drawable.slider1,R.drawable.slider2, R.drawable.slider3, R.drawable.slider4};
+		placesList.add(place);
+
+		place = new Place();
+		places.name = Chicago;
+		places.imageResource = {R.drawable.slider1,R.drawable.slider2, R.drawable.slider3, R.drawable.slider4};
+		placesList.add(place);
+
+	}
+
 /*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -133,4 +170,160 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 			e.printStackTrace();
 		}
 	}
+
+	private void AnimateandSlideShow() {
+		try
+		{
+			final Animation rotateimage = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+			final Animation fadeOut = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+
+			fadeOut.setAnimationListener(new AnimationListener() {
+
+				@Override
+				public void onAnimationStart(Animation animation) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void onAnimationEnd(Animation animation) {
+
+					AnimateandSlideShow();
+				}
+			});
+
+			rotateimage.setAnimationListener(new AnimationListener() {
+
+				@Override
+				public void onAnimationStart(Animation animation) {
+
+
+				}
+
+				@Override
+				public void onAnimationRepeat(Animation animation) {
+
+
+				}
+
+				@Override
+				public void onAnimationEnd(Animation animation) {
+
+					//slidingimage.setImageResource(IMAGE_IDS[currentimageindex%IMAGE_IDS.length]);
+
+					new Timer().schedule(new TimerTask() {
+						@Override
+						public void run() {
+							runOnUiThread(new Runnable() {
+
+								@Override
+								public void run() {
+									// TODO Auto-generated method stub
+									slidingimage.startAnimation(fadeOut);
+								}
+							});
+						}
+					}, 5000);
+				}
+			});
+
+			currentimageindex++;
+			imgLoader.displayImage(IMAGE_IDS.get(currentimageindex % IMAGE_IDS.size()), slidingimage, options, new ImageLoadingListener() {
+				@Override
+				public void onLoadingStarted(String imageUri, View view) {
+
+				}
+
+				@Override
+				public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+				}
+
+				@Override
+				public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+					slidingimage.startAnimation(rotateimage);
+				}
+
+				@Override
+				public void onLoadingCancelled(String imageUri, View view) {
+				}
+
+			}, new ImageLoadingProgressListener() {
+				@Override
+				public void onProgressUpdate(String imageUri, View view, int current, int total) {
+				}
+			});
+			// slidingimage.setImageResource(IMAGE_IDS.get(currentimageindex%IMAGE_IDS.size()));
+
+
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	public class ImageAdapter extends BaseAdapter {
+		@Override
+		public int getCount() {
+			return placesList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			final ViewHolder holder;
+			View view = convertView;
+			try {
+
+				if (view == null) {
+					view = getLayoutInflater().inflate(R.layout.list_item_layout, parent, false);
+					holder = new ViewHolder();
+					assert view != null;
+
+					holder.imageView = (ImageView) view.findViewById(R.id.imageViewPhotos);
+					holder.cityName = (TextView) view.findViewById(R.id.textViewCityName);
+
+					view.setTag(holder);
+				} else {
+					holder = (ViewHolder) view.getTag();
+				}
+
+				ResponseClass res = responseClassArrayList.get(position);
+
+				if(res != null){
+					holder.blogName.setText(res.blog_name);
+					if(res.photos.size() > 0){
+						imgLoader.displayImage(res.photos.get(0), holder.imageView, options);
+						holder.photos.setText(String.valueOf(res.photos.size()) + " photos");
+					}
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			return view;
+		}
+
+		class ViewHolder {
+			ImageView imageView;
+			TextView cityName;
+		}
+	}
+
 }
